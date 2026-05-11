ef814e93e6595143afac5e17c4aaf6397e59f908
EngArsany <arsanyo2003@gmail.com>
2026-05-11 21:35:09 +0300

    Add hard difficulty skeleton

--- src/ai/ai.py
+++ src/ai/ai.py
@@ -3,7 +3,7 @@
 from enum import Enum
 from game.player import Player
 from game.board import Board
-from game.rules import get_valid_moves
+from game.rules import get_valid_moves, get_valid_walls
 from ai.bfs_distance import bfs_distance
 from typing import Tuple
 
@@ -28,7 +28,7 @@ def get_config(mode : str):
         }
     else: # Hard
         return{
-            "depth": 5,
+            "depth": 4,
             "weights": (15, 10, 5),
             "search_function" : hard_search
 
@@ -63,7 +63,7 @@ def get_best_move(board, ai, human, mode):
 
     return best_move
 
-# Evaulating score
+# === Evaulating score ===
 def evaluate(board: Board, ai: Player, human: Player, weights : Tuple[int, int, int]):
     distance_w, wall_w, mobility_w = weights
 
@@ -81,6 +81,9 @@ def evaluate(board: Board, ai: Player, human: Player, weights : Tuple[int, int, 
 
     return distance_score + wall_score + mobility_score
 
+def evaluate_wall():
+    pass
+
 # === Searching best move ===
 def easy_search(board, ai, human, config):
     return minimax(board, human, ai, False, config["depth"] - 1,
@@ -91,7 +94,25 @@ def medium_search(board, ai, human, config):
                         float('-inf'), float('inf'), config["weights"])
 
 def hard_search(board, ai, human, config):
-    pass
+    best_pawn_move = None
+    best_pawn_score = -float('-inf')
+    
+    for pawn_move in get_valid_moves(board, ai, human):
+        pass
+        # Apply move temporarily
+        # Call minimax(depth=4, using A* evaluation)
+        # Track best
+    
+    best_wall = get_best_wall(board, ai, human)
+    best_wall_score = evaluate_wall(board, ai, human, best_wall)
+    
+    # Step 3: Compare and decide
+    if ai.walls_left > 0 and best_wall_score > best_pawn_score:
+        # Execute wall placement (need to modify board)
+        # Return special signal or place wall directly
+        return best_wall  # or (best_wall, is_wall=True)
+    else:
+        return best_pawn_move
 
 # === Algorithms ===
 def minimax(board: Board, human: Player, ai: Player, maximise: bool,
@@ -142,4 +163,10 @@ def minimax(board: Board, human: Player, ai: Player, maximise: bool,
                 break
 
         return best
+
+def a_star_distance():
+    pass
+
+def get_best_wall():
+    pass
 
